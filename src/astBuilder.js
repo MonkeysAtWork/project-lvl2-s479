2637c657d41c10b3727029f3ef07c5fe81c562a0
M.Fredd <sk3250@mail.ru>
2020-04-07 13:00:11 +0500

    refactoring builder

--- src/astBuilder.js
+++ src/astBuilder.js
@@ -33,6 +33,14 @@ const propertyActions = [
 const getPropertyAction = (arg1, arg2) => propertyActions.find(({ check }) => check(arg1, arg2));
 
 
+const buildNode = (key, value1, value2, fn) => {
+  const { type, getContent } = getPropertyAction(value1, value2);
+  const content = getContent(value1, value2, fn);
+
+  return { type, key, ...content };
+};
+
+
 const getDiff = (data1 = {}, data2 = {}) => {
   const uniqKeys = _.union(Object.keys(data1), Object.keys(data2));
 
@@ -40,10 +48,7 @@ const getDiff = (data1 = {}, data2 = {}) => {
     const oldValue = _.has(data1, key) ? data1[key] : '';
     const newValue = _.has(data2, key) ? data2[key] : '';
 
-    const { type, getContent } = getPropertyAction(oldValue, newValue);
-    const content = getContent(oldValue, newValue, getDiff);
-
-    return { type, key, ...content };
+    return buildNode(key, oldValue, newValue, getDiff);
   });
 };
 
